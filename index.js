5167e7b7eee9f67a9275f69045df13514c5c2b30
taillan <taillandier.mathieu@outlook.fr>
2023-03-06 08:58:14 +0100

    avacnement

--- index.js
+++ index.js
@@ -28,8 +28,80 @@ client.on("disconnected", function () {
 
 client.on('messageCreate', msg => {
 // You can view the msg object here with console.log(msg)
-    console.log(msg.content)
-    if (msg.content === 'Hello') {
-            msg.reply(`Hello ${msg.author.username}`);
+    entry_price = 0;
+    leverage = 1;
+    type = "";
+    stop_loss = 0;
+    tp_list = [];
+    token="";
+
+    Parsed = msg.content.split('\n')
+    if (Parsed[0] === '-------------------------') {
+            for(const el in Parsed){
+              if(Parsed[el].includes("Token")){
+                let temp =Parsed[el].split(' ');
+                token = temp[temp.length -1];
+              }
+              if(Parsed[el].includes("Type")){
+                let temp =Parsed[el].split(' ');
+                type = temp[temp.length -1];
+              }
+              if(Parsed[el].includes("Entry price")){
+                let temp =Parsed[el].split(' ');
+                entry_price = temp[temp.length -1].replace('$','');
+              }
+              if(Parsed[el].includes("TP")){
+                let temp =Parsed[el].split(' ');
+                tp_list.push(temp[temp.length -1].replace('$',''));
+              }
+              if(Parsed[el].includes("Stop Loss")){
+                let temp =Parsed[el].split(' ');
+                stop_loss = temp[temp.length -1].replace('$','');
+              }
+            }
+
+            if(entry_price<0.009){
+              leverage=10;
+            }
+            else if(entry_price<0.09){
+              leverage=15;
+            }
+            else if(entry_price<10){
+              leverage=17.5;
+            }
+            else if(entry_price<50){
+              leverage=20;
+            }
+            else if(entry_price<100){
+              leverage=20;
+            }
+            else if(entry_price<500){
+              leverage=25;
+            }
+            else if(entry_price>5000){
+              leverage=30;
+            }
+            
+            let POSITION =
+            {
+              "category": "linear",
+              "symbol": token.replace("/",""),
+              "tradeMode":1, //0: cross margin. 1: isolated margin
+              "buyLeverage": leverage,
+              "buyLeverage": leverage,
+              "tpSlMode": "Partial", //Full,Partial
+              "stopLoss": stop_loss.toString(),
+              "slSize": stop_loss.toString(),
+            }
+
+            
+            msg.reply(`entry_price ${entry_price}`);
+            msg.reply(`leverage ${leverage}`);
+            msg.reply(`type ${type}`);
+            msg.reply(`stop_loss ${stop_loss}`);
+            msg.reply(`tp_list ${tp_list}`);
+            msg.reply(`token ${token}`);
         }
+
+        
 });
