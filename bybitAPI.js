b3463034ed475bcade1a0200583e1c1d9e4473c3
Taillandier <77755557+Taillan@users.noreply.github.com>
2023-03-09 22:02:23 +0100

    .js pour toi

--- bybitAPI.js
+++ bybitAPI.js
@@ -1,39 +1,40 @@
 const {
-    InverseClient,
-    LinearClient,
-    InverseFuturesClient,
-    SpotClientV3,
-    UnifiedMarginClient,
-    USDCOptionClient,
-    USDCPerpetualClient,
-    AccountAssetClient,
-    CopyTradingClient,
-    RestClientV5,
-  } = require('bybit-api');
+  InverseClient,
+  LinearClient,
+  InverseFuturesClient,
+  SpotClientV3,
+  UnifiedMarginClient,
+  USDCOptionClient,
+  USDCPerpetualClient,
+  AccountAssetClient,
+  CopyTradingClient,
+  RestClientV5,
+} = require("bybit-api");
 
-  var fs = require("fs");
+var fs = require("fs");
 
-  const API_KEY = fs.readFileSync("../.api_key").toString('utf-8');
-  const API_SECRET = fs.readFileSync("../.api_secret", "utf8");;
-  const useTestnet = false;
-  
-  const client = new RestClientV5({
-      key: API_KEY,
-      secret: API_SECRET,
-      testnet: useTestnet,
-      enable_time_sync: 1,
-      },
-    );
-  
-  const getAllCountBallanceContract = () => {
-    client.getAllCoinsBalance({
-      accountType: 'CONTRACT'})
-    .then(result => {
+const API_KEY = fs.readFileSync("../.api_key").toString("utf-8");
+const API_SECRET = fs.readFileSync("../.api_secret", "utf8");
+const useTestnet = false;
+
+const client = new RestClientV5({
+  key: API_KEY,
+  secret: API_SECRET,
+  testnet: useTestnet,
+  enable_time_sync: 1,
+});
+
+const getAllCountBallanceContract = () => {
+  client
+    .getAllCoinsBalance({
+      accountType: "CONTRACT",
+    })
+    .then((result) => {
       return result.result;
     })
-    .catch(err => {
+    .catch((err) => {
       return err;
     });
-  }
+};
 
-module.export = {getAllCountBallanceContract};
+module.export = { getAllCountBallanceContract };
--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-require('dotenv').config(); //initialize dotenv
-const { getAllCountBallanceContract } = require('./bybitAPI');
-const {
-  messageLink
-} = require('discord.js');
-const Discord = require('discord.js');
-const axios = require('axios');
+require("dotenv").config(); //initialize dotenv
+const { getAllCountBallanceContract } = require("./bybitAPI.js");
+
+const { messageLink } = require("discord.js");
+const Discord = require("discord.js");
+const axios = require("axios");
 var fs = require("fs");
 
-const {
-  type
-} = require('os');
-endpoint = require('./endpoint.js');
-
-//GET la clef API depuis un fichier privé
-var crypto = require("crypto");
-
-var axios_instance = axios.create({
-  baseURL: "https://api-testnet.bybit.com",
-  timeout: 1000
-});
+endpoint = require("./endpoint.js");
 
 const client = new Discord.Client({
   intents: [
@@ -27,26 +15,24 @@ const client = new Discord.Client({
     Discord.GatewayIntentBits.MessageContent,
     Discord.GatewayIntentBits.DirectMessages,
     Discord.GatewayIntentBits.Guilds,
-  ]
+  ],
 });
-
-
-const sign = crypto.createSign('RSA-SHA256');
 
 client.login(process.env.TOKEN); //login bot using token
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-  });
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-client.on("messageCreate", msg => {
+client.on("messageCreate", (msg) => {
   if (msg.content.includes("Balance")) {
-    msg.reply("```JSON\n" + JSON.stringify(getAllCountBallanceContract()) + "```");
+    msg.reply(
+      "```JSON\n" + JSON.stringify(getAllCountBallanceContract()) + "```"
+    );
   }
-}
-)
+});
 
-client.on('messageCreate', msg => {
+client.on("messageCreate", (msg) => {
   entry_price = 0;
   leverage = 1;
   trade_type = "";
@@ -54,33 +40,33 @@ client.on('messageCreate', msg => {
   tp_list = [];
   token = "";
 
-  Parsed = msg.content.split('\n')
-  if (Parsed[0] === '-------------------------') {
+  Parsed = msg.content.split("\n");
+  if (Parsed[0] === "-------------------------") {
     for (const el in Parsed) {
       if (Parsed[el].includes("Token")) {
-        let temp = Parsed[el].split(' ');
+        let temp = Parsed[el].split(" ");
         token = temp[temp.length - 1];
       }
       if (Parsed[el].includes("Type")) {
-        let temp = Parsed[el].split(' ');
+        let temp = Parsed[el].split(" ");
         trade_type = temp[temp.length - 1];
         if (trade_type == "Long") {
           trade_type = "Buy";
         } else {
-          trade_type = "Sell"
+          trade_type = "Sell";
         }
       }
       if (Parsed[el].includes("Entry price")) {
-        let temp = Parsed[el].split(' ');
-        entry_price = temp[temp.length - 1].replace('$', '');
+        let temp = Parsed[el].split(" ");
+        entry_price = temp[temp.length - 1].replace("$", "");
       }
       if (Parsed[el].includes("TP")) {
-        let temp = Parsed[el].split(' ');
-        tp_list.push(temp[temp.length - 1].replace('$', ''));
+        let temp = Parsed[el].split(" ");
+        tp_list.push(temp[temp.length - 1].replace("$", ""));
       }
       if (Parsed[el].includes("Stop Loss")) {
-        let temp = Parsed[el].split(' ');
-        stop_loss = temp[temp.length - 1].replace('$', '');
+        let temp = Parsed[el].split(" ");
+        stop_loss = temp[temp.length - 1].replace("$", "");
       }
     }
 
@@ -101,25 +87,24 @@ client.on('messageCreate', msg => {
     }
 
     let POSITION = {
-      "category": "linear",
-      "symbol": token.replace("/", ""),
-      "isLeverage": 1,
-      "side": trade_type,
-      "orderType": "Market", //TODO Ecris en dure a modifié
-      "qty": "a calculer avec le entryprice et les 5% du wallet (selon strat)",
-      "price": entry_price,
-      "takeProfit": "A gérer car on peux en mettre quun mais ne pas mettre la position a 100%",
-      "stopLoss": stop_loss.toString(),
-      "slTriggerBy": "A voir avec un trader la stratégie",
+      category: "linear",
+      symbol: token.replace("/", ""),
+      isLeverage: 1,
+      side: trade_type,
+      orderType: "Market", //TODO Ecris en dure a modifié
+      qty: "a calculer avec le entryprice et les 5% du wallet (selon strat)",
+      price: entry_price,
+      takeProfit:
+        "A gérer car on peux en mettre quun mais ne pas mettre la position a 100%",
+      stopLoss: stop_loss.toString(),
+      slTriggerBy: "A voir avec un trader la stratégie",
       // La suite a vérifier cest pas partout dans la doc a essayer sur le testnet quoi
-      "tradeMode": 1, //0: cross margin. 1: isolated margin
-      "buyLeverage": leverage,
-      "buyLeverage": leverage,
-      "tpSlMode": "Full", //Full,Partial
-      "slSize": stop_loss.toString(),
-    }
+      tradeMode: 1, //0: cross margin. 1: isolated margin
+      buyLeverage: leverage,
+      buyLeverage: leverage,
+      tpSlMode: "Full", //Full,Partial
+      slSize: stop_loss.toString(),
+    };
     msg.reply("```JSON\n" + JSON.stringify(POSITION) + "```");
   }
-
-
 });
